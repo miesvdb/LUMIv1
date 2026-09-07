c035c81a030a08bb533ab2b239cdc476ab2ac573
miesvdb <Marisajoosten@outlook.com>
2026-09-07 14:00:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="lumi-home-consistency-v1-12";
+const CACHE="lumi-ux-round1-safe-v14";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{
